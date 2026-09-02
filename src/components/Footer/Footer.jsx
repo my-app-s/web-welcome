6f57efbdc50c034a176116501949b4a9c2c0e54b
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-09-02 21:30:02 +0000

    refactor(footer): simplify layout by removing unused links and updating copyright styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,9 @@ import './Footer.css'
 function Footer() {
     return(
         <footer>
-            <div className="container container--flex">
+            <div className="container">
                 <section className="copyright">
                     © Copyright 2026 my-app-s(M.R.E). Content rights belong to the author.
-                </section>
-                <section className="footer-text">
-                    <a href="#nav">
-                        Contact & GitHub
-                    </a>
                 </section>
             </div>
         </footer>
