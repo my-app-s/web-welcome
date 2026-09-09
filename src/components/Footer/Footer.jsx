1d0c50677e54b440aefac50f30dccce6c7603f33
M.R.E.(Muratbekov Rustem) <94853425+my-app-s@users.noreply.github.com>
2026-09-09 20:56:55 +0500

    Refactor Footer component layout and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <footer>
-            <div className="mx-auto max-w-2xl px-4 text-sm text-custom-github-text">
-                <section className="flex h-auto min-h-6 flex-wrap items-center justify-center bg-black py-2 text-center leading-tight">
+            <div className="mx-auto max-w-2xl px-4 text-sm">
+                <section className="flex h-auto min-h-6 flex-wrap items-center justify-center py-2 text-center leading-tight">
                     © Copyright 2026 my-app-s(M.R.E) Content rights belong to the author.
                 </section>
             </div>
